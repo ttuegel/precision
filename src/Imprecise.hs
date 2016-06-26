f26dc5ebda3be186145e591b862bb5c5e9c3edc8
Thomas Tuegel <ttuegel@gmail.com>
2016-06-26 08:58:21 -0500

    rename package to `imprecise' and simplify interface

---
+++ src/Imprecise.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TypeFamilies #-}
+
+
+module Imprecise ( Imprecise(..) ) where
+
+import Foreign.C.Types ( CDouble, CFloat )
+import Numeric.IEEE ( epsilon )
+
+
+class Imprecise a where
+    type Precision a
+    precision :: a -> Precision a
+    limiting :: a -> Precision a
+    scale :: Precision a -> a -> a
+
+
+instance Imprecise Double where
+    type Precision Double = Double
+    precision x = abs x * epsilon
+    limiting = abs
+    scale = (*)
+
+
+instance Imprecise Float where
+    type Precision Float = Float
+    precision x = abs x * epsilon
+    limiting = abs
+    scale = (*)
+
+
+instance Imprecise CDouble where
+    type Precision CDouble = CDouble
+    precision x = abs x * epsilon
+    limiting = abs
+    scale = (*)
+
+
+instance Imprecise CFloat where
+    type Precision CFloat = CFloat
+    precision x = abs x * epsilon
+    limiting = abs
+    scale = (*)
