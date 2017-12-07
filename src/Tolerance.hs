55ec165cb9de1919be49626d08a503bd50a4bb3d
Thomas Tuegel <ttuegel@mailbox.org>
2017-12-07 14:42:57 -0600

    Fix interface for composite floating point types
    For composite floating point types, the tolerance must be defined for each
    component in case the magnitude of components differs greatly. As a side effect,
    the interface is vastly simplified.

--- src/Tolerance.hs
+++ src/Tolerance.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE UndecidableSuperClasses #-}
 {-
 
 Copyright 2017 Thomas Tuegel
@@ -27,71 +23,48 @@ import Foreign.C.Types ( CDouble, CFloat )
 import Numeric.IEEE ( epsilon )
 
 
-class (Num (Tol a), Ord (Tol a)) => Tolerance a where
-    type Tol a
-    consistent :: Tol a -> a -> a -> Bool
+class Num a => Tolerance a where
+    -- | Compare two values for equality up to the specified tolerance.
+    consistent
+        :: a  -- ^ tolerance
+        -> a
+        -> a
+        -> Bool
 
-
-class Tolerance a => Precision a where
-    precision :: a -> Tol a
-
-
-class Tolerance a => Accuracy a where
-    accuracy :: a -> a -> Tol a
+    -- | The precision at a value is the smallest representable increment.
+    -- The type admits no values between @x@ and @x + precision x@
+    -- or between @x - precision x@ and @x@.
+    precision
+        :: a  -- ^ value
+        -> a  -- ^ tolerance
 
 
 instance Tolerance Double where
-    type Tol Double = Double
     consistent tol x y = abs (x - y) <= tol
 
-instance Precision Double where
     precision x = abs x * epsilon
-
-instance Accuracy Double where
-    accuracy x y = abs (x - y)
 
 
 instance Tolerance Float where
-    type Tol Float = Float
     consistent tol x y = abs (x - y) <= tol
 
-instance Precision Float where
     precision x = abs x * epsilon
-
-instance Accuracy Float where
-    accuracy x y = abs (x - y)
 
 
 instance Tolerance CDouble where
-    type Tol CDouble = CDouble
     consistent tol x y = abs (x - y) <= tol
 
-instance Precision CDouble where
     precision x = abs x * epsilon
-
-instance Accuracy CDouble where
-    accuracy x y = abs (x - y)
 
 
 instance Tolerance CFloat where
-    type Tol CFloat = CFloat
     consistent tol x y = abs (x - y) <= tol
 
-instance Precision CFloat where
     precision x = abs x * epsilon
 
-instance Accuracy CFloat where
-    accuracy x y = abs (x - y)
 
+instance (RealFloat a, Tolerance a) => Tolerance (Complex a) where
+    consistent (ret :+ imt) (re0 :+ im0) (re1 :+ im1) =
+        consistent ret re0 re1 && consistent imt im0 im1
 
-instance Tolerance a => Tolerance (Complex a) where
-    type Tol (Complex a) = Tol a
-    consistent tol (re0 :+ im0) (re1 :+ im1) =
-        consistent tol re0 re1 && consistent tol im0 im1
-
-instance Precision a => Precision (Complex a) where
-    precision (re :+ im) = max (precision re) (precision im)
-
-instance Accuracy a => Accuracy (Complex a) where
-    accuracy (re0 :+ im0) (re1 :+ im1) =
-        max (accuracy re0 re1) (accuracy im0 im1)
+    precision (re :+ im) = precision re :+ precision im
